67858462a583e688104d4de8cb0f768abb82b0d2
aneeshmaury <aneeshmaury.in@gmail.com>
2026-05-29 13:26:56 +0530

    egergt

--- app/page.js
+++ app/page.js
@@ -48,7 +48,7 @@ export default function PaymentRequestUI() {
 
           {/* UPI ID */}
           <h1
-            className="text-center text-[21px] leading-none font-[700] text-[#171717] break-all"
+            className="text-center text-[18px] leading-none tracking-wide font-[700] text-[#171717] break-all"
             style={{ fontFamily: "'Poppins', sans-serif" }}
           >
             {upiId}
@@ -58,14 +58,14 @@ export default function PaymentRequestUI() {
           <div className="mt-3 bg-[#eef2f6] rounded-[22px] py-3 px-4">
             
             <p
-              className="text-center text-[18px] text-[#787878] font-[400] leading-none"
+              className="text-center text-[16px] tracking-wide text-[#787878] font-[400] leading-none"
               style={{ fontFamily: "'Poppins', sans-serif" }}
             >
               Business Name
             </p>
 
             <h2
-              className="text-center text-[18px] font-[700] text-[#1d1d1d] mt-2 leading-none break-words"
+              className="text-center tracking-wide text-[17px] font-[700] text-[#1d1d1d] mt-1 leading-none break-words"
               style={{ fontFamily: "'Poppins', sans-serif" }}
             >
               {name}
@@ -77,7 +77,7 @@ export default function PaymentRequestUI() {
       {/* Bottom Text */}
       <div className="px-8 mt-3 flex justify-center">
         <p
-          className="max-w-[760px] text-center text-[#3f3f3f] text-[14px] leading-[17px] font-[500]"
+          className="max-w-[760px] text-center text-[#3f3f3f] text-[12px] leading-[17px] font-[500]"
           style={{ fontFamily: "'Poppins', sans-serif" }}
         >
           This merchant receives money through payment requests. To pay again,
